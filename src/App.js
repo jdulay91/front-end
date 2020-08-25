91cdc3a9ecd66e8cbf9fa9153f2d5d850f618435
Julliann Dulay <jullianndulay@gmail.com>
2020-08-25 12:22:19 -0700

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState} from "react";
 import Header from "./components/Header";
 import { Route, useHistory } from "react-router-dom";
 import Login from "./components/Login";
